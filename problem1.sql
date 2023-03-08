a550de4361ecab820f7395b182c739b994f3e1f5
Bernardgc25 <Bernard_gcruz@yahoo.com>
2023-03-08 03:19:57 +0000

    completed: Insert

---
+++ problem1.sql
@@ -0,0 +1,16 @@
+   
+ /* 
+    CREATE TABLE song (
+           Title varchar(100),
+           Artist varchar(100)
+        );
+  */
+  
+   INSERT INTO song (Title, Artist)
+   VALUES ('Let it be', 'Beatles');
+
+   INSERT INTO song (Title, Artist)
+   VALUES ('Hotel California', 'Eagles');
+   
+   INSERT INTO song (Title, Artist)
+   VALUES ('Kashmir', 'Led Zeppelin');
